ae1db3d52ff80d1ccd99fb3a02ac9bb00e19c6d3
vagrant <vagrant@homestead>
2020-10-29 19:20:07 +0000

    cambios en footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -9,12 +9,12 @@
             </div>
         </div>
         <div class="container" id="footer2">
-            <ul>    
+            <ul class="redes">    
                 <li><a href="https://www.instagram.com/lyrasalud/?hl=es-la"><img src="images/INSTAGRAM.png" class="img-footer" > </a> <a href="https://www.instagram.com/lyrasalud/?hl=es-la">@LyraSalud</a> </li>
                 <li><a href="https://api.whatsapp.com/send?phone=542214973663"><img src="images/WHATSAPP.png" class="img-footer" > </a><a href="https://api.whatsapp.com/send?phone=542214973663">0221 3550 6989 </a> </li>
                 <li><a href="https://www.facebook.com/lyra.salud.10"> <img src="images/FACEBOOK.png" class="img-footer" ></a><a href="https://www.facebook.com/lyra.salud.10">@LyraSalud </a> </li>
             </ul>
-            <ul>
+            <ul class="redes"> 
                 <li><a><img src="images/address.png" class="img-footer"> </a><a>60 n°1124 </a> </li>
                 <li><a><img src="images/tel.png" class="img-footer"> </a><a>0221 3550 6989 </a> </li>
                 <li><a href="mailto:ventas@lyrasalud.com"><img src="images/gmail.png" class="img-footer"> </a><a href="mailto:ventas@lyrasalud.com">ventas@lyrasalud.com </a> </li>
